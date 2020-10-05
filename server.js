980890aad26d8d8498b9907ef787e7128c81bd3c
Salem <salemshahdeve@gmail.com>
2020-10-05 16:23:00 +0200

    some changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const data = [
+const users = [
     {id: 1, name: "jan"},
     {id: 2, name: "khan"},
     {id: 3, name: "baba"},
@@ -10,13 +10,17 @@ const server = http.createServer((request, response) => {
     const {headers, url, method} = request
     //console.log("Headers ===>", headers, "Url===>", url, "Method ===> ", method);
     if (method === "POST" || method === "GET") {
-        response.setHeader('content-Type', 'application/json')
+        response.setHeader('content-Type', 'json')
+        //response.setHeader('content-Type', 'text/html')
         response.setHeader('X-Powered-By', 'Node.js')
         //response.write("<h2>This is just for testing</h2>")
     }
     response.end(JSON.stringify({
         success: true,
-        data: data,
+        url,
+        method,
+        headers,
+        users,
     }));
 });
 
