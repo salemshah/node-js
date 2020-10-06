2912cd48d3c802290f1e1c3acf04010d50de497a
salem <salemshah686@gmail.com>
2020-10-07 00:29:28 +0200

    get data from body request

--- server.js
+++ server.js
@@ -10,11 +10,27 @@ const server = http.createServer((request, response) => {
   const { headers, url, method } = request;
   //console.log("Headers ===>", headers, "Url===>", url, "Method ===> ", method);
   if (method === "POST" || method === "GET") {
-    response.setHeader("content-Type", "application/json");
+    //response.statusCode = 404;
+    //response.setHeader("content-Type", "application/json");
     //response.setHeader('content-Type', 'text/html')
-    response.setHeader("X-Powered-By", "Node.js");
+    //response.setHeader("X-Powered-By", "Node.js");
     //response.write("<h2>This is just for testing</h2>")
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+    });
   }
+
+  //request.headers.authorization;// get data form headers request
+  let body = [];
+
+  request
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+    });
   response.end(
     JSON.stringify({
       success: true,
@@ -22,6 +38,7 @@ const server = http.createServer((request, response) => {
       method,
       headers,
       users,
+      auto: request.headers.authorization,
     })
   );
 });
