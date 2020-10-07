5e67b76640c9b05f20e06506231f8db024ef1c6c
Salem <salemshahdeve@gmail.com>
2020-10-07 11:41:55 +0200

    routing by nodeJs get data from user

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const http = require("http");
+const users = [
+  { id: 1, name: "jan" },
+  { id: 2, name: "khan" },
+  { id: 3, name: "baba" },
+];
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-  response.end()
+  const { method, url } = request
+
+  let body = []
+  request.on('data', chunk => {
+    body.push(chunk)
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+
+    let status = 404;
+    const responses = {
+      success: false,
+      data: null
+    };
+
+    if (method === "GET" && url === "/users") {
+      status = 200;
+      responses.success = true;
+      responses.data = users
+    } else if (method === "POST" && url === "/users") {
+      const { id, name } = JSON.parse(body)
+      if (!id || !name) {
+        status = 400;
+      } else {
+        users.push({ id, name });
+        status = 201;
+        responses.success = true;
+        responses.data = users;
+        responses.countUsers = Object.keys(responses.data).length;
+      }
+
+    }
+
+    response.writeHead(status, {
+      "Content-Type": "application/json",
+    });
+    response.end(JSON.stringify(responses))
+
+  })
+
+
 });
 
 const PORT = 5000;
